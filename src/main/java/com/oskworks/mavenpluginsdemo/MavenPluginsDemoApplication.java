9ad163435b3b8b5de46ebac8309ce589ac4cdbee
fullee <icngor@163.com>
2020-04-01 21:43:40 +0800

    init

---
+++ src/main/java/com/oskworks/mavenpluginsdemo/MavenPluginsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.oskworks.mavenpluginsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenPluginsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenPluginsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oskworks/mavenpluginsdemo/MavenPluginsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oskworks.mavenpluginsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenPluginsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
