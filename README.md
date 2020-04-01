518c95ad84de6ba7d2d0b318ce2a6b8bd724ab63
fullee <icngor@163.com>
2020-04-01 22:03:56 +0800

    add git-commit-id plugin

---
+++ README.md
@@ -0,0 +1,10 @@
+# 插件介绍
+
+1. git-commit-id
+```shell script
+mvn clean package
+curl http://localhost:8080/version
+```
+
+
+
--- src/main/java/com/oskworks/mavenpluginsdemo/MavenPluginsDemoApplication.java
+++ src/main/java/com/oskworks/mavenpluginsdemo/MavenPluginsDemoApplication.java
@@ -2,6 +2,13 @@ package com.oskworks.mavenpluginsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @SpringBootApplication
 public class MavenPluginsDemoApplication {
